690ebc613ec11e6b381043ba1941538aa229bd6c
Mona Lisa <imanolvalenzuela03@gmail.com>
2023-12-28 18:14:01 -0300

    Genero Interfaz y Agrego Funcionalidades

---
+++ src/programa/AgendaElectronica.java
@@ -0,0 +1,10 @@
+package programa;
+import igu.Principal;
+
+public class AgendaElectronica {
+    public static void main(String[] args) {
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
